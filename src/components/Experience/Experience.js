badf8db6b6926166d1edd6831990765e4d395a46
Ty4jbt <tyaboe@gmail.com>
2021-09-15 19:16:42 -0700

    added experience components

--- src/components/Experience/Experience.js
+++ src/components/Experience/Experience.js
@@ -3,8 +3,48 @@ import './Experience.css';
 
 const Experience = () => {
     return (
-        <div>
-            
+        <div className='experience'>
+            <div className="d-flex justify-content-center my-5">
+                <h1>experience</h1>
+            </div>
+            <div className="container experience-wrapper">
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker">
+
+                    </div>
+                    <div className="timeline-content">
+                        <h3>August 2020 - November 2020</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sint velit quis nobis maxime, omnis sed doloremque neque repudiandae commodi distinctio. Minus dignissimos provident excepturi? Alias, aperiam nesciunt consequatur corporis at repellat voluptatem, quibusdam beatae quae esse dolor porro fuga!</p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker">
+
+                    </div>
+                    <div className="timeline-content">
+                        <h3>August 2020 - November 2020</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sint velit quis nobis maxime, omnis sed doloremque neque repudiandae commodi distinctio. Minus dignissimos provident excepturi? Alias, aperiam nesciunt consequatur corporis at repellat voluptatem, quibusdam beatae quae esse dolor porro fuga!</p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-right">
+                    <div className="marker">
+
+                    </div>
+                    <div className="timeline-content">
+                        <h3>August 2020 - November 2020</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sint velit quis nobis maxime, omnis sed doloremque neque repudiandae commodi distinctio. Minus dignissimos provident excepturi? Alias, aperiam nesciunt consequatur corporis at repellat voluptatem, quibusdam beatae quae esse dolor porro fuga!</p>
+                    </div>
+                </div>
+                <div className="timeline-block timeline-block-left">
+                    <div className="marker">
+
+                    </div>
+                    <div className="timeline-content">
+                        <h3>August 2020 - November 2020</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos sint velit quis nobis maxime, omnis sed doloremque neque repudiandae commodi distinctio. Minus dignissimos provident excepturi? Alias, aperiam nesciunt consequatur corporis at repellat voluptatem, quibusdam beatae quae esse dolor porro fuga!</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
